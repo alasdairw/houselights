31e509f86a6715d99a826fa8358dc8f32c3a539d
Alasdair Watson <alasdair@alasdair.biz>
2015-09-06 10:42:03 +0000

    Setting management, and some really basic unit tests

---
+++ database/seeds/LightSettingsTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\LightSetting;
+
+class LightSettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        function csv_to_array($filename='', $delimiter=',')
+        {
+            if(!file_exists($filename) || !is_readable($filename))
+                return FALSE;
+
+            $header = NULL;
+            $data = array();
+            if (($handle = fopen($filename, 'r')) !== FALSE)
+            {
+                while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+                {
+                    if(!$header)
+                        $header = $row;
+                    else
+                        $data[] = array_combine($header, $row);
+                }
+                fclose($handle);
+            }
+            return $data;
+        }
+
+      
+
+        $csvFile = public_path().'/../resources/basic_light_settings.csv';
+
+        $settings = csv_to_array($csvFile);
+        foreach($settings as $setting)
+        {
+            $color_data = explode(',',str_replace('(','',str_replace(')','',$setting['R,G,B'])));
+            $red = $color_data[0];
+            $green = $color_data[1];
+            $blue = $color_data[2];
+            $xy = json_decode($setting['XY']);
+            LightSetting::create(['name'=>$setting['Name'],'red'=>$red,'green'=>$green,'blue'=>$blue,'x'=>$xy[0],'y'=>$xy[1]]);
+        }
+    }
+}
---
+++ tests/HouseLightTest.php
@@ -0,0 +1,96 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+use App\HouseLight;
+use App\LightSetting;
+use App\Jobs\TurnLightOn;
+use App\Jobs\TurnLightOff;
+use App\Jobs\SetLightColor;
+
+class HouseLightTest extends TestCase
+{
+    use DispatchesJobs;
+
+    /**
+     * This isn't ideal - it assumes a correctly set up database, and accessible lights on network
+     * TODO: Better set up.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->createApplication();
+        $lights = HouseLight::all();
+        foreach($lights as $light)
+        {
+            $job = new TurnLightOff($light->light_id);
+        }
+
+    }
+
+    //use DatabaseTransactions, DatabaseMigrations;
+    /**
+     * Tests what happens when a light that is off is toggled on.
+     *
+     * @return void
+     */
+    public function testLightOn()
+    {
+        $light = HouseLight::where('light_id', 1)->first();
+        $this->assertInstanceOf('App\HouseLight',$light);
+        $job = new TurnLightOn($light->light_id);
+        $this->dispatch($job);
+        $this->seeInDatabase('lights', ['light_id' => $light->light_id,'state'=> '1']);
+    }
+
+    /**
+     * Tests what happens when a light that is on is toggled off.
+     * @return void
+     */
+    public function testLightOff()
+    {
+        $light = HouseLight::where('light_id', 1)->first();
+        $this->assertInstanceOf('App\HouseLight',$light);
+        $job = new TurnLightOff($light->light_id);
+        $this->dispatch($job);
+        $this->seeInDatabase('lights', ['light_id' => $light->light_id,'state'=> '0']);    
+    }
+
+    public function testLightColorChange()
+    {
+        $this->testLightOn();
+        
+        $light = HouseLight::where('light_id', 1)->first();
+        $this->assertInstanceOf('App\HouseLight',$light);
+        
+        $color = LightSetting::all()->random(1);
+        $this->assertInstanceOf('App\LightSetting',$color);
+
+        $job = new SetLightColor($light->light_id,$color->x,$color->y);
+        $this->dispatch($job);
+        $this->seeInDatabase('lights', ['light_id' => $light->light_id,'state'=> '1','xy'=>$color->x.','.$color->y]);
+        $this->testLightOff();
+
+
+    }
+
+    /**
+     * Turn off all lights after test.
+     * TODO: It'd be nice to save a pre-test state and return to that.
+     */
+    public function tearDown()
+    {
+        $lights = HouseLight::all();
+        foreach($lights as $light)
+        {
+            $job = new TurnLightOff($light->light_id);
+        }
+        $this->dispatch($job);
+        parent::tearDown();
+        
+    }
+
+}
